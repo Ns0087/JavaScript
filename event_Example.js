c38fe0b427fae904e5dce1aadd10b4f755d84e81
NS0087 <nitinsaraswat.30@gmail.com>
2022-12-15 14:20:25 +0530

    event example

---
+++ event_Example.js
@@ -0,0 +1,26 @@
+const EventEmiter = require('events');
+const fs = require('fs');
+const event = new EventEmiter();
+
+event.on('abc', ()=>{
+    var r = fs.readFileSync("abc.txt");
+    console.log(r.toString());
+});
+
+event.on('abc', ()=>{
+    var r = fs.writeFileSync('abc.txt', 'This is Event Exampe');
+    console.log('File Written');
+})
+
+//Event Emit
+event.emit('abc');
+console.log("end");
+
+const os = require('os');
+// var x = 1024*1024*1024;
+// console.log(os.freemem()/x);
+const tfree = os.totalmem();
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+
